2cb44f89d34cc0d81edad122456093256a7ad290
Lucila Paiva <lucila.paiva@gmail.com>
2023-04-02 04:04:06 -0300

    Probando la función de ocultar el contador en js. Comencé el responsive en Experiencia, falta resolver la tabla y el footer en Experiencia.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//alert('Bienvenido a mi portfolio Model Web');
+const submitButton = document.getElementById("submitButton");
+const contador = document.getElementById("contador");
+
+submitButton.addEventListener('submit', () => {
+
+    contador.classList.remove("visually-hidden-focusable");
+console.log('hola')
+
+});
