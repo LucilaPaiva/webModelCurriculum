fbe6895b231180ac33253318fadc2969535c16a9
Lucila Paiva <lucila.paiva@gmail.com>
2023-04-04 14:11:36 -0300

    contador terminado

--- js/script.js
+++ js/script.js
@@ -1,59 +1,33 @@
-function cuentaRegresiva() {
 
-    // Fecha actual en la zona horaria de Buenos Aires
-    var fechaActual = new Date().getTime();
+function countdown(targetDate) {
+    let daysElement = document.getElementById("days");
+    let hoursElement = document.getElementById("hours");
+    let minutesElement = document.getElementById("minutes");
+    let secondsElement = document.getElementById("seconds");
+    
+    let countdownInterval = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = targetDate - now;
+      
+      if (distance < 0) {
+        clearInterval(countdownInterval);
+        daysElement.innerText = "0";
+        hoursElement.innerText = "0";
+        minutesElement.innerText = "0";
+        secondsElement.innerText = "0";
+        // Aquí puedes agregar la lógica que desees al finalizar la cuenta regresiva
+        return;
+      }
+      
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      daysElement.innerText = days;
+      hoursElement.innerText = hours;
+      minutesElement.innerText = minutes;
+      secondsElement.innerText = seconds;
+    }, 1000);
+  }
 
-    // Fecha objetivo (1 de julio de 2023) en la zona horaria de Buenos Aires
-    var fechaObjetivo = new Date("July 1, 2023 00:00:00 GMT-0300").getTime();
-
-    // Diferencia de tiempo entre la fecha actual y la fecha objetivo
-    var diferencia = fechaObjetivo - fechaActual;
-
-    // Conversión de la diferencia de tiempo a días, horas, minutos y segundos
-    var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    var horas = Math.floor(
-        (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-    var segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
-
-    // Actualización del contenido del div con la cuenta regresiva
-    document.getElementById("cuentaAtras").innerHTML =
-        "<div class='div-contador'>" +
-
-            //div con los días
-            "<div class='dias'>Días: " +
-                dias +
-            "</div>" +
-
-            //div con las horas
-            "<div class='horas'>Horas: " +
-                horas +
-            "</div>" +
-
-            //div con los minutos
-            "<div class='minutos'>Minutos: " +
-                minutos +
-            "</div>" +
-
-            //div con los segundos
-            "<div class='segundos'>Segundos: " +
-                segundos +
-            "</div>" +
-
-        "</div>"
-        
-        ;
-
-    // Si la cuenta regresiva ha llegado a cero, se detiene la actualización
-    if (diferencia < 0) {
-        clearInterval(intervalo);
-        document.getElementById("cuentaAtras").innerHTML =
-            "¡La cuenta regresiva ha terminado!";
-    }
-}
-
-document.getElementById("contador").onclick = function () {
-    // Llamada a la función "cuentaRegresiva" cada segundo
-    var intervalo = setInterval(cuentaRegresiva, 1000);
-};
